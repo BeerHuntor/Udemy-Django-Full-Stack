f6dc2ce9dda882da4cb6b21aed6311d4800ddf7b
HuntorGames <rt001a9138@gmail.com>
2024-04-05 22:58:10 +0100

    updated settings.py with template search directory BASE_DIR using import os, and updated urls.py

--- DJANGO-LEVEL-2-TEMPLATES/django_templates/django_templates/urls.py
+++ DJANGO-LEVEL-2-TEMPLATES/django_templates/django_templates/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('my_app/', include('my_app.urls')),
+
 ]
+
+handler404 = 'django_templates.views.my_custom_page_not_found_view'
