0919591e3caa7350edc65881040a8c563e2aa354
HuntorGames <rt001a9138@gmail.com>
2024-04-06 16:12:31 +0100

    went over adding aditional fields to the models when needed and using djangos validators to validate entries

--- DJANGO-LEVEL-3-MODELS-AND-DATABASES/Dentist_Website/office/models.py
+++ DJANGO-LEVEL-3-MODELS-AND-DATABASES/Dentist_Website/office/models.py
@@ -1,9 +1,13 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 class Patient(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    age = models.IntegerField()
-
+    age = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(120)])
+    # Adding fields after the model has been created requries an default value for all previous fields already in the DB
+    heart_rate = models.IntegerField(default=60, validators=[MinValueValidator(1), MaxValueValidator(300)]) 
     
+
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}, and is {self.age} years old!"
