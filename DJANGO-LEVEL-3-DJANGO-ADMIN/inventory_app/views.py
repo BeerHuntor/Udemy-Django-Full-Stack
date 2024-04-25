1165ec9f3064298cdd8bb13f27e89164b3b70f64
HuntorGames <rt001a9138@gmail.com>
2024-04-25 12:30:25 +0100

    Added functionality to the form for adding cars to the database, and linked up the front end with the backend, with a POST request and create.models call.

--- DJANGO-LEVEL-3-DJANGO-ADMIN/inventory_app/views.py
+++ DJANGO-LEVEL-3-DJANGO-ADMIN/inventory_app/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
+from . import models
 
 template_dir = 'inventory_app/'
 # Create your views here.
 def list(request):
-    return render(request, 'inventory_app/list.html')
+    all_cars = models.Car.objects.all()
+    context = {'all_cars':all_cars}
+    return render(request, 'inventory_app/list.html', context = context)
 
 def delete(request):
     return render(request, 'inventory_app/delete.html')
 
 def add(request):
+    if request.POST:
+        brand = request.POST['brand']
+        year = int(request.POST['year'])
+        models.Car.objects.create(brand=brand, year=year)
     return render(request, 'inventory_app/add.html')
