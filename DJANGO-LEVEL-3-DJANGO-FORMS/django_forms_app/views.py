96e7def4af0007102494013695ad1c64a05b6d3e
HuntorGames <rt001a9138@gmail.com>
2024-04-25 15:06:59 +0100

    Set up basic views and set the settings.py file with the required configurations.

--- DJANGO-LEVEL-3-DJANGO-FORMS/django_forms_app/views.py
+++ DJANGO-LEVEL-3-DJANGO-FORMS/django_forms_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_rental_review(request):
+    return render(request, 'app_templates/rental_review')
+
+def show_thank_you(request):
+    return render(request, 'app_templates/thank-you.html')
