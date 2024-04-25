559d1c9d1ae5dc8f8c2497495fc9dcb2c4ead354
HuntorGames <rt001a9138@gmail.com>
2024-04-25 14:02:15 +0100

    Customising the admin panel, and setting a super user via manage.py createsuperuser command in the terminal.  Different variations how to show and edit in the admin panel

--- DJANGO-LEVEL-3-DJANGO-ADMIN/inventory_app/admin.py
+++ DJANGO-LEVEL-3-DJANGO-ADMIN/inventory_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from inventory_app.models import Car
 
 # Register your models here.
+
+#One liner if you don't need the model class object. 
+#admin.site.register(Car)
+
+class CarAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('TIME_INFORMATION', {'fields':['year']}), 
+        ('CAR_INFORMATION', {'fields':['brand']}),
+    ]
+admin.site.register(Car, CarAdmin)
