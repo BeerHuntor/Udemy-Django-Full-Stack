4a157479e5a33574b691f5761421439cf53eea60
HuntorGames <rt001a9138@gmail.com>
2024-04-25 15:22:58 +0100

    Added paths to link the project level templates

--- DJANGO-LEVEL-3-DJANGO-FORMS/django_forms_app/urls.py
+++ DJANGO-LEVEL-3-DJANGO-FORMS/django_forms_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from django_forms_app.views import show_rental_review, show_thank_you
 
 urlpatterns = [
-
+    path('', show_rental_review, name='rental_review'),
+    path('thank-you/', show_thank_you, name='thank_you'),
 ]
--- DJANGO-LEVEL-3-DJANGO-FORMS/django_forms_app/views.py
+++ DJANGO-LEVEL-3-DJANGO-FORMS/django_forms_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def show_rental_review(request):
-    return render(request, 'app_templates/rental_review')
+    return render(request, 'app_templates/rental-review.html')
 
 def show_thank_you(request):
     return render(request, 'app_templates/thank-you.html')
