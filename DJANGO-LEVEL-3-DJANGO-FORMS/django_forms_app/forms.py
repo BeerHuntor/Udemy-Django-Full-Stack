3c688d8c53917f58a86f11a2491df6f269a3f8b5
HuntorGames <rt001a9138@gmail.com>
2024-04-26 16:52:44 +0100

    Updated the project to use 'widgets and styling' and learned how to style pythonically within the code istelf, and how to connect each field to css.

--- DJANGO-LEVEL-3-DJANGO-FORMS/django_forms_app/forms.py
+++ DJANGO-LEVEL-3-DJANGO-FORMS/django_forms_app/forms.py
@@ -4,4 +4,5 @@ class ReviewForm(forms.Form):
     first_name = forms.CharField(label='First Name', max_length= 30, required=False)
     last_name = forms.CharField(label='Last Name', max_length= 30, required=False)
     email = forms.EmailField(label='Email', required=False)
-    review_contnet = forms.CharField(label='Tell us what you think.', required=False)
+    review_contnet = forms.CharField(label='Tell us what you think.', required=False,
+                                     widget=forms.Textarea(attrs={'class': 'myform', 'rows': '10','cols':'120', 'placeholder': "Text goes here!"}))
--- DJANGO-LEVEL-3-DJANGO-FORMS/django_forms_site/settings.py
+++ DJANGO-LEVEL-3-DJANGO-FORMS/django_forms_site/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_URL = '/static/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -115,8 +116,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
+# Changed from the standard 'STATIC_URL' field, into a collection. 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
