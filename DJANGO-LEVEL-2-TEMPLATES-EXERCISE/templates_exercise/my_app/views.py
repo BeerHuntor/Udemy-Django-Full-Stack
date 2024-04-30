59ea9424dd5ca4ab302dfdb8462f30cdb7519d37
HuntorGames <rt001a9138@gmail.com>
2024-04-30 23:21:57 +0100

    commiting deletions

--- DJANGO-LEVEL-2-TEMPLATES-EXERCISE/templates_exercise/my_app/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def help_view(request):
-    help_page = {'help' : 'help page'}
-    return render (request, 'my_app/help.html', context=help_page)
--- DJANGO-LEVEL-3-MODELS-AND-DATABASES/app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from . import models
-
-BASE_DIR = '/Dentist_Website/'
-# Create your views here.
-def list_patients(request):
-    all_patients = models.Patient.objects.all()
-    context = {'patients':all_patients}
-    return render(request, 'office/list.html', context=context )
