e4c66ae6c82e228b240cf8efceede19ce59fb281
HuntorGames <rt001a9138@gmail.com>
2024-05-01 13:22:29 +0100

    linked the urls and the views up to be displayed on their individual pages

--- DJANGO-LEVEL-2-MODELS-EXERCISE/ProTwo_app/urls.py
+++ DJANGO-LEVEL-2-MODELS-EXERCISE/ProTwo_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index_view
+from .views import index_view, help_view, users_view
 
 urlpatterns = [
     path('', index_view, name='index_view'),
+    path('help/', help_view, name='help_view'),
+    path('users/', users_view, name='users_view')
 ]
--- DJANGO-LEVEL-2-MODELS-EXERCISE/ProTwo_app/views.py
+++ DJANGO-LEVEL-2-MODELS-EXERCISE/ProTwo_app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from ProTwo_app.models import Users
 
 # Create your views here.
 def index_view(request):
-    render(request, 'app/x.html')
+    return render(request, 'app/index.html')
+
+def help_view(request):
+    return render(request, 'app/help.html')
+
+def users_view(request):
+    all_users = Users.objects.all()
+    users = {'users_view' : all_users}
+    return render(request, 'app/users.html', context=users)
