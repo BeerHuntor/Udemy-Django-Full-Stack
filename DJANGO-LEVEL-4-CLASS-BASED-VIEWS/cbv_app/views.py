1dd2d41f57c8eb07a3a253462ef9da80d4419fe2
HuntorGames <rt001a9138@gmail.com>
2024-05-01 21:58:54 +0100

    using reverse_lazy we can reverse the url and link to a urlpattern

--- DJANGO-LEVEL-4-CLASS-BASED-VIEWS/cbv_app/views.py
+++ DJANGO-LEVEL-4-CLASS-BASED-VIEWS/cbv_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView, FormView
 
 from cbv_app.forms import ContactForm
@@ -14,7 +14,7 @@ class ContactFormView(FormView):
     form_class = ContactForm
     template_name = "app/contact.html"
 
-    success_url = '/thank_you/'# This is a URL as in your browser, not a pattern/pointer.
+    success_url = reverse_lazy('thank_you')# This is a URL as in your browser, not a pattern/pointer.
 
     def form_valid(self, form):
         print(form.cleaned_data)
