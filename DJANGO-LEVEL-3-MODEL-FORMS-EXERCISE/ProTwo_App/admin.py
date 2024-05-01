9cce50f12412469cdbdd79d26532c18882260e6e
HuntorGames <rt001a9138@gmail.com>
2024-05-01 16:17:55 +0100

    registered the user model to the admin panel

--- DJANGO-LEVEL-3-MODEL-FORMS-EXERCISE/ProTwo_App/admin.py
+++ DJANGO-LEVEL-3-MODEL-FORMS-EXERCISE/ProTwo_App/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from ProTwo_App.models import User
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    fields = ["first_name", "last_name", "email"]
+
+admin.site.register(User, UserAdmin)
