6dc33ba0e68e3f0dc68235bbe6834fc7ed331820
HuntorGames <rt001a9138@gmail.com>
2024-05-01 20:54:15 +0100

    created a new thank_you.html page and TemplateView, to link to thank you page from a link on the index page

--- DJANGO-LEVEL-4-CLASS-BASED-VIEWS/cbv_app/urls.py
+++ DJANGO-LEVEL-4-CLASS-BASED-VIEWS/cbv_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from cbv_app.views import HomeTemplateView
+from cbv_app.views import HomeTemplateView, ThankYouTemplateView
 
 urlpatterns = [
     path('', HomeTemplateView.as_view(), name='index'),
+    path('thank_you/', ThankYouTemplateView.as_view(), name='thank_you')
 ]
--- DJANGO-LEVEL-4-CLASS-BASED-VIEWS/cbv_app/views.py
+++ DJANGO-LEVEL-4-CLASS-BASED-VIEWS/cbv_app/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
-class  HomeTemplateView(TemplateView):
+class HomeTemplateView(TemplateView):
     template_name = 'app/index.html'
+
+class ThankYouTemplateView(TemplateView):
+    template_name = 'app/thank_you.html'
