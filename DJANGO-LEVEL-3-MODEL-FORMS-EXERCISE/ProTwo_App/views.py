1da6b2104c06414f307967adaa3e4b72969b9c77
HuntorGames <rt001a9138@gmail.com>
2024-05-01 16:01:23 +0100

    saved the form on a post request to the database

--- DJANGO-LEVEL-3-MODEL-FORMS-EXERCISE/ProTwo_App/views.py
+++ DJANGO-LEVEL-3-MODEL-FORMS-EXERCISE/ProTwo_App/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from ProTwo_App.forms import UserModelForm
 
 # Create your views here.
 def index_view(request):
     return render(request, 'app/index.html')
 
 def sign_up_view(request):
-    return render(request, 'app/users.html')
+    if request.method == "POST":
+        form = UserModelForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('index_view')
+        
+    else:
+        form = UserModelForm()
+        return render(request, 'app/users.html', {'form':form})
 
 def help_view(request):
     return render(request, 'app/help.html')
