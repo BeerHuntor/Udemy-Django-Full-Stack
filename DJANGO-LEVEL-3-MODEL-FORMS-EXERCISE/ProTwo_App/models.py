7d07bafb7302f1e51f6c0d8bdd8f1fc1e0a2aba4
HuntorGames <rt001a9138@gmail.com>
2024-05-01 15:59:51 +0100

    forgot the inheritance call

--- DJANGO-LEVEL-3-MODEL-FORMS-EXERCISE/ProTwo_App/models.py
+++ DJANGO-LEVEL-3-MODEL-FORMS-EXERCISE/ProTwo_App/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class User():
+class User(models.Model):
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
     email = models.EmailField(max_length=254)
