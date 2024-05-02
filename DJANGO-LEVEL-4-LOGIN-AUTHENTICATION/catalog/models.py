270905f5efe816d6a167d423d1340424e03396f2
HuntorGames <rt001a9138@gmail.com>
2024-05-02 16:15:51 +0100

    added slug field to the model, and overwrote the save function to set the slug to the book_title slugified and return the url based on the slug

--- DJANGO-LEVEL-4-LOGIN-AUTHENTICATION/catalog/models.py
+++ DJANGO-LEVEL-4-LOGIN-AUTHENTICATION/catalog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 
+from django.template.defaultfilters import slugify
+
 import uuid
 # Create your models here.
 class Genre(models.Model):
@@ -20,6 +22,7 @@ class Language(models.Model):
 class Book(models.Model):
     # model representing a book. 
     book_title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True) # This means if someone somehow accidently deletes the author, the book still exists, and author just returns NULL
     publish_date = models.DateField(auto_now=False, auto_now_add=True)
     summary = models.TextField(max_length=1000)
@@ -27,12 +30,16 @@ class Book(models.Model):
     genre = models.ManyToManyField(Genre) # book can have a list of potential genres
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
     
+    def save(self, *args, **kwargs):
+        #Generate slug based on title
+        self.slug = slugify(self.book_title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.book_title}'
     
     def get_absolute_url(self):
-        return reverse('book_detail', kwargs={'pk': self.pk})
+        return reverse('book_detail', kwargs={'slug': self.slug})
 
 
 class Author(models.Model):
