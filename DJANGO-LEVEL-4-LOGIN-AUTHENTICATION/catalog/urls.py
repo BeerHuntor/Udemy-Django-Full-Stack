efd40025e90748d948fe5ae686a334cad995b236
HuntorGames <rt001a9138@gmail.com>
2024-05-02 16:16:48 +0100

    changed the dynamic url to look for a slug rather than the <int:pk>

--- DJANGO-LEVEL-4-LOGIN-AUTHENTICATION/catalog/urls.py
+++ DJANGO-LEVEL-4-LOGIN-AUTHENTICATION/catalog/urls.py
@@ -4,6 +4,6 @@ from catalog.views import CatalogHomeView, BookCreate, BookDetail
 urlpatterns = [
     path('', CatalogHomeView, name='catalog_home'),
     path('create_book/', BookCreate.as_view(), name='create_book'),
-    path('book_detail/<int:pk>/', BookDetail.as_view(), name='book_detail')
+    path('book_detail/<slug:slug>/', BookDetail.as_view(), name='book_detail')
     
 ]
