ea38f520a25218d10424aa60ccac13d7c5ab9b75
HuntorGames <rt001a9138@gmail.com>
2024-05-02 13:38:49 +0100

    pulled some data from the database table to show in the template

--- DJANGO-LEVEL-4-LOGIN-AUTHENTICATION/catalog/views.py
+++ DJANGO-LEVEL-4-LOGIN-AUTHENTICATION/catalog/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from catalog.models import Book, Author, BookInstance, Genre,Language
 
 # Create your views here.
 def CatalogHomeView(request):
-    return render(request, 'catalog/index.html')
+
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_avail = BookInstance.objects.filter(status__exact='a').count()
+    
+    context = {
+        'num_books' : num_books,
+        'num_instances' : num_instances,
+        'num_instances_avail' : num_instances_avail,
+    }
+
+    return render(request, 'catalog/index.html', context=context)
