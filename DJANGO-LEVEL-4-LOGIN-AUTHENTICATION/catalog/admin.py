c27dd93a733e1dc2715f2d84646d4b8466990f50
HuntorGames <rt001a9138@gmail.com>
2024-05-02 13:19:14 +0100

    registered the models with the admin panel

--- DJANGO-LEVEL-4-LOGIN-AUTHENTICATION/catalog/admin.py
+++ DJANGO-LEVEL-4-LOGIN-AUTHENTICATION/catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from catalog.models import Genre, Author, Book, BookInstance, Language
 
 # Register your models here.
+admin.site.register(Genre)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(BookInstance)
+admin.site.register(Language)
