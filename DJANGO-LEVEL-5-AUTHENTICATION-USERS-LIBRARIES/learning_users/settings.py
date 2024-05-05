ad1af1afc9e50c5ace96bdf0d67025d6725e0d81
HuntorGames <rt001a9138@gmail.com>
2024-05-05 21:56:56 +0100

    changed STATIC_DIRS to STATICFILES_DIRS as the css file wasn't being found.

--- DJANGO-LEVEL-5-AUTHENTICATION-USERS-LIBRARIES/learning_users/settings.py
+++ DJANGO-LEVEL-5-AUTHENTICATION-USERS-LIBRARIES/learning_users/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_DIRS = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static/'),]
 
 # Media - This folder is used for users who upload to the website, such as profile picture etc, as apposed to static, where they are, our images for website creation. 
 MEDIA_ROOT = MEDIA_DIR
