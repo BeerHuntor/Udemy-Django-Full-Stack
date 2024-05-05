337fdd640016e0f2031da56e35eab178af9fb330
HuntorGames <rt001a9138@gmail.com>
2024-05-05 14:29:54 +0100

    linked up profile url pattern

--- DJANGO-LEVEL-4-LOGIN-AUTHENTICATION/catalog/urls.py
+++ DJANGO-LEVEL-4-LOGIN-AUTHENTICATION/catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from catalog.views import CatalogHomeView, BookCreate, BookDetail, my_view, SignUpView
+from catalog.views import CatalogHomeView, BookCreate, BookDetail, my_view, SignUpView, CheckedOutBooksByUser
 
 urlpatterns = [
     path('', CatalogHomeView, name='catalog_home'),
     path('create_book/', BookCreate.as_view(), name='create_book'),
     path('book_detail/<slug:slug>/', BookDetail.as_view(), name='book_detail'),
     path('my_view/', my_view, name='my_view'),
-    path('signup/', SignUpView.as_view(), name="sign_up")    
+    path('signup/', SignUpView.as_view(), name="sign_up"),
+    path('profile/', CheckedOutBooksByUser.as_view(), name='profile'),    
 ]
