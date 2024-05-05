889594bcee97cecb8e44b9602de22503d87888c9
HuntorGames <rt001a9138@gmail.com>
2024-05-05 17:58:03 +0100

    updated requirements.txt with pillow library, and created accompanying model to the base user model, by creating a relationship with the base model class.

--- DJANGO-LEVEL-5-AUTHENTICATION-USERS-LIBRARIES/authapp/models.py
+++ DJANGO-LEVEL-5-AUTHENTICATION-USERS-LIBRARIES/authapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfileInfo(models.Model):
+    # Create relationship, (don't inherity from User!)
+    user = models.OneToOneField(User)
+
+    # Add any additional attributes you want. 
+    user_portfolio = models.URLField(blank=True, max_length=200)
+    user_profile_image = models.ImageField(upload_to='profile_pics')
+
+    def __str__(self):
+        pass
