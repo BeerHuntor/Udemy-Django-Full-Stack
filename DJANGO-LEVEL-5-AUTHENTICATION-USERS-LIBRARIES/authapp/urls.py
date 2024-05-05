048ef3815f40c2e84fa5e3d51223ae098a22e46d
HuntorGames <rt001a9138@gmail.com>
2024-05-06 00:55:16 +0100

    added login form to login.html

--- DJANGO-LEVEL-5-AUTHENTICATION-USERS-LIBRARIES/authapp/urls.py
+++ DJANGO-LEVEL-5-AUTHENTICATION-USERS-LIBRARIES/authapp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', show_index_view, name='index'),
     path('login/', show_login_view, name='login'), 
     path('register/', RegisterFormView.as_view(), name='register'),
-    path('logout/',show_logout_view, name='logout' )
+    path('logout/',show_logout_view, name='logout'),
 ]
