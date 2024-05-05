f16a886e2b2687f6b6fd31c933dc43f5526846da
HuntorGames <rt001a9138@gmail.com>
2024-05-05 18:52:09 +0100

    Created 2 forms,  for the user, and the additional info

--- DJANGO-LEVEL-5-AUTHENTICATION-USERS-LIBRARIES/authapp/forms.py
+++ DJANGO-LEVEL-5-AUTHENTICATION-USERS-LIBRARIES/authapp/forms.py
@@ -1,10 +1,20 @@
 from django import forms 
+from django.contrib.auth.models import User
 from authapp.models import UserProfileInfo
 
-class USerProfileInfoForm(forms.ModelForm):
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(), max_length=25,required=True)
+
+    class Meta():
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileInfoForm(forms.ModelForm):
     portfolio = forms.URLField(required=False)
     profile_picture = forms.ImageField(required=False)
     
     class Meta():
         model = UserProfileInfo
-        exclude = ('user')
+        fields = ('portfolio', 'profile_picture')
+ 
