00411850de13e5e698bc6af888e93b0cd9296920
HuntorGames <rt001a9138@gmail.com>
2024-05-05 13:29:20 +0100

    linked up the SignUpView class based view

--- DJANGO-LEVEL-4-LOGIN-AUTHENTICATION/catalog/urls.py
+++ DJANGO-LEVEL-4-LOGIN-AUTHENTICATION/catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from catalog.views import CatalogHomeView, BookCreate, BookDetail, my_view
+from catalog.views import CatalogHomeView, BookCreate, BookDetail, my_view, SignUpView
 
 urlpatterns = [
     path('', CatalogHomeView, name='catalog_home'),
     path('create_book/', BookCreate.as_view(), name='create_book'),
     path('book_detail/<slug:slug>/', BookDetail.as_view(), name='book_detail'),
-    path('my_view/', my_view, name='my_view'),    
+    path('my_view/', my_view, name='my_view'),
+    path('signup/', SignUpView.as_view(), name="sign_up")    
 ]
