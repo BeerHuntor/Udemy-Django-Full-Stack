b85060ec703c3f15dd67aa4e5a569e60af3cae56
HuntorGames <rt001a9138@gmail.com>
2024-05-05 22:30:37 +0100

    added url patterns for register and login templates

--- DJANGO-LEVEL-5-AUTHENTICATION-USERS-LIBRARIES/authapp/urls.py
+++ DJANGO-LEVEL-5-AUTHENTICATION-USERS-LIBRARIES/authapp/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from authapp.views import show_index_view, show_login_view, RegisterFormView
+
+APP_NAME = 'authapp'
 
 urlpatterns = [
-    
+    path('', show_index_view, name='index'),
+    path('login', show_login_view, name='login'), 
+    path('register/', RegisterFormView.as_view(), name='register'),
 ]
