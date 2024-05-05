d01fcf4109fc7f4503b98cc08438f958937520ed
HuntorGames <rt001a9138@gmail.com>
2024-05-05 22:32:02 +0100

    added views to just get the ball rolling, added registratio form as FormView CBV and plugged that into the registration.html template via overriding its context

--- DJANGO-LEVEL-5-AUTHENTICATION-USERS-LIBRARIES/authapp/views.py
+++ DJANGO-LEVEL-5-AUTHENTICATION-USERS-LIBRARIES/authapp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
+from authapp.models import User, UserProfileInfo
+from authapp.forms import UserForm, UserProfileInfoForm
 # Create your views here.
+def show_index_view(request):
+    return render(request, 'authapp/index.html')
+
+def show_login_view(request):
+    return render(request, 'authapp/login.html')
+
+class RegisterFormView(FormView):
+    form_class = UserForm
+    other_form_class = UserProfileInfoForm
+    success_url = '/'   
+
+    def get_template_names(self): 
+        return ['authapp/registration.html']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_form'] = self.form_class
+        context['profile_form'] = self.other_form_class
+        return context
+    
+    def get_success_url(self):
+        return reverse_lazy('register')
