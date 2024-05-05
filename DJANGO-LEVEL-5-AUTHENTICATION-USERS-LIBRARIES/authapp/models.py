f7444b8ff9de746b09847f9cde70305a55f63412
HuntorGames <rt001a9138@gmail.com>
2024-05-05 18:52:43 +0100

    fixed some param's

--- DJANGO-LEVEL-5-AUTHENTICATION-USERS-LIBRARIES/authapp/models.py
+++ DJANGO-LEVEL-5-AUTHENTICATION-USERS-LIBRARIES/authapp/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import User
 
 class UserProfileInfo(models.Model):
     # Create relationship, (don't inherity from User!)
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
 
     # Add any additional attributes you want. 
     user_portfolio = models.URLField(blank=True, max_length=200)
-    user_profile_image = models.ImageField(upload_to='profile_pics')
+    user_profile_image = models.ImageField(upload_to='profile_pics', blank=True)
 
     def __str__(self):
         return self.user.username
