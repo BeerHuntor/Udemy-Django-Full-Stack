db097fd98e9e029a62c7c24232d59909bde99c64
HuntorGames <rt001a9138@gmail.com>
2024-05-06 00:17:41 +0100

    added logout url pattern

--- DJANGO-LEVEL-5-AUTHENTICATION-USERS-LIBRARIES/authapp/urls.py
+++ DJANGO-LEVEL-5-AUTHENTICATION-USERS-LIBRARIES/authapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from authapp.views import show_index_view, show_login_view, RegisterFormView
+from authapp.views import show_index_view, show_login_view, show_logout_view, RegisterFormView
 
 APP_NAME = 'authapp'
 
 urlpatterns = [
     path('', show_index_view, name='index'),
-    path('login', show_login_view, name='login'), 
+    path('login/', show_login_view, name='login'), 
     path('register/', RegisterFormView.as_view(), name='register'),
+    path('logout/',show_logout_view, name='logout' )
 ]
