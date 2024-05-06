23331aa26d62ee435bbaef74a1d2b27dfbc9f914
HuntorGames <rt001a9138@gmail.com>
2024-05-06 01:14:47 +0100

    commiting changes

--- DJANGO-LEVEL-5-AUTHENTICATION-USERS-LIBRARIES/authapp/urls.py
+++ DJANGO-LEVEL-5-AUTHENTICATION-USERS-LIBRARIES/authapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from authapp.views import show_index_view, show_login_view, show_logout_view, RegisterFormView
 
-APP_NAME = 'authapp'
+app_name = 'authapp'
 
 urlpatterns = [
     path('', show_index_view, name='index'),
     path('login/', show_login_view, name='login'), 
-    path('register/', RegisterFormView.as_view(), name='register'),
     path('logout/',show_logout_view, name='logout'),
+    path('register/', RegisterFormView.as_view(), name='register'),
+
 ]
